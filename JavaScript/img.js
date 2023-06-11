594cadab35a87dfe826d00e817362d728105c34e
westkite23 <yocv09456@gmail.com>
2023-06-11 14:32:43 +0900

    first commit

---
+++ JavaScript/img.js
@@ -0,0 +1,16 @@
+const imgs = document.querySelectorAll('.imgs img')
+const current = document.getElementById('current')
+
+imgs.forEach((img) => img.addEventListener('click', imgClick))
+
+function imgClick(e) {
+  imgs.forEach((img) => (img.style.opacity = 1))
+
+  current.src = e.target.src
+
+  current.classList.add('fade-in')
+
+  setTimeout(() => current.classList.remove('fade-in'), 500)
+
+  e.target.style.opacity = 0.4
+}
